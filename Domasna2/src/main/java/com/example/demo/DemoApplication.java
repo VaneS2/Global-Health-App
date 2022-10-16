9b072c7c677bc866a04e6d7e32be221cf4153401
VaneS2 <74139708+VaneS2@users.noreply.github.com>
2022-10-16 02:59:04 +0200

    .
    .

--- Domasna2/src/main/java/com/example/demo/DemoApplication.java
+++ Domasna2/src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,62 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 @SpringBootApplication
 @ServletComponentScan
 public class DemoApplication {
 
+    public static class Home {
+        public  void printf()
+        {
+            System.out.println("Home");
+        }
+
+
+    }
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+         class  Apartment extends Home {
+            public  void printf()
+            {
+                System.out.println("Apartment");
+            }
+
+        }
+
+         class House extends Home {
+            public  void printf()
+            {
+                System.out.println("House");
+            }
+
+
+        }
+        class Haha  {
+            public  void printf()
+            {
+                System.out.println("House");
+            }
+
+
+        }
+
+        ArrayList<Home> homes= new ArrayList<>();
+        Apartment apartment= new Apartment();
+        homes.add(new Home());
+        homes.add(new House());
+
+        for(int i=0;i<homes.size();i++)
+        {
+            homes.get(i).printf();
+        }
+
     }
 
 }
